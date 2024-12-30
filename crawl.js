2e030c19426a3fc4b679f73ea82c979ad387b3c3
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2024-12-30 20:14:17 +0530

    updated tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,44 @@
-function normalizeURL(urlString) {
-  const urlobj = new URL(`https://luxxblogsite.netlify.app/path`)
+const { JSDOM } = require('jsdom')      // importing jsdom package
+function getURLsFromHtml(htmlBody, baseURL) { // baseURL is the url where the web crawler is gonna run
+const urls = []
+  const dom = new JSDOM(htmlBody)
+const linkElements =  dom.window.document.querySelectorAll('a')
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === '/') /* checks for the presence of '/'
+as the relative url is a path*/    {
+ // concatenating  relative url with base url
+        try {
+          const urlobj = new URL(`${baseURL}${linkElement.href}`)
+          urls.push(urlobj.href)
+        }
+  catch (err) {
+    console.log(`error with relative url: ${err.message}`)
+  }
+    } else {
+      //absolute url
+   try {
+     const urlobj = new URL(linkElement.href)
+          urls.push(urlobj.href)
+        }
+  catch (err) {
+    console.log(`error with absolute  url: ${err.message}`)
+  }
+    }
+  }
+  return urls
+
+}
+function normalizeURL(urlString) {   /* To standardise the url,
+it ensures that all URLs are treated as same*/
+  const urlobj = new URL(urlString)
   const kumar = `${urlobj.hostname}${urlobj.pathname}`
-if(kumar.length > 0 && kumar.slice(-1) === '/') {
+if(kumar.length > 0 && kumar.slice(-1) === '/') { // checks if the url has / at the end
   return kumar.slice(0, -1)
 }
-return kumar
+return kumar   // kumar will return the normalized URL
 }
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHtml
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,12 +1,14 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, sunni, getURLsFromHtml } = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
+
 test('normalizeURL strip protocol', () => {
-  const input = 'https://luxxblogsite.netlify.app/path/'
+  const input = 'https://luxxblogsite.netlify.app/path'
   const actual = normalizeURL(input)
   const expected = 'luxxblogsite.netlify.app/path'
  expect(actual).toEqual(expected)
 })
+
 test('normalizeURL strip trailing slash', () => {
   const input = 'https://luxxblogsite.netlify.app/path/'
   const actual = normalizeURL(input)
@@ -20,10 +22,78 @@ test('normalizeURL strip capitals', () => {
   const expected = 'luxxblogsite.netlify.app/path'
  expect(actual).toEqual(expected)
 })
+
 test('normalizeURL strip http', () => {
   const input = 'http://luxxblogsite.netlify.app/path '
   const actual = normalizeURL(input)
   const expected = 'luxxblogsite.netlify.app/path'
  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+  const inputHTMLBody = `
+  <html>
+  <body>
+  <a href="https://luxxblogsite.netlify.app/path/">
+       luxxblogsite.netlify.app
+       </a>
+       </body>
+       </html>
+       `
+  const inputBaseURL = "https://luxxblogsite.netlify.app/path/"
+  const actual = getURLsFromHtml(inputHTMLBody, inputBaseURL)
+    const expected = ["https://luxxblogsite.netlify.app/path/"]
+expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+  const inputHTMLBody = `
+  <html>
+  <body>
+  <a href="/path/">
+       luxxblogsite.netlify.app
+       </a>
+       </body>
+       </html>
+       `
+  const inputBaseURL = "https://luxxblogsite.netlify.app"
+  const actual = getURLsFromHtml(inputHTMLBody, inputBaseURL)
+    const expected = ["https://luxxblogsite.netlify.app/path/"]
+expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+  const inputHTMLBody = `
+  <html>
+  <body>
+ <a href="https://luxxblogsite.netlify.app/path1/">
+       luxxblogsite.netlify.app Path one
+    </a>
+  <a href="/path2/">
+       luxxblogsite.netlify.app Path two
+      </a>
+      </body>
+       </html>
+       `
+  const inputBaseURL = "https://luxxblogsite.netlify.app"
+  const actual = getURLsFromHtml(inputHTMLBody, inputBaseURL)
+    const expected = ["https://luxxblogsite.netlify.app/path1/","https://luxxblogsite.netlify.app/path2/"]
+expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+  const inputHTMLBody = `
+  <html>
+  <body>
+  <a href="invalid">
+       Invalid URL
+       </a>
+       </body>
+       </html>
+       `
+  const inputBaseURL = "https://luxxblogsite.netlify.app"
+  const actual = getURLsFromHtml(inputHTMLBody, inputBaseURL)
+    const expected = []
+expect(actual).toEqual(expected)
 })
 
